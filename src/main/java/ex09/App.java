4dee8bf8728922852f9939c945727d50a23a64e1
savosburn <savosburn18@tampabay.rr.com>
2021-06-04 11:08:39 -0400

    Initial commit

---
+++ src/main/java/ex09/App.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
+package ex09;
+
+/*
+Sometimes you have to round up to the next number rather than follow standard rounding rules.
+
+Calculate gallons of paint needed to paint the ceiling of a room.
+Prompt for the length and width, and assume one gallon covers 350 square feet.
+Display the number of gallons needed to paint the ceiling as a whole number.
+
+Example Output
+You will need to purchase 2 gallons of paint to cover 360 square feet.
+Remember, you can’t buy a partial gallon of paint. You must
+round up to the next whole gallon.
+
+Constraints
+Use a constant to hold the conversion rate.
+Ensure that you round up to the next whole number.
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
